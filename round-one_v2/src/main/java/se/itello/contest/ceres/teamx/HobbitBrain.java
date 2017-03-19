92d22032912c3a769ab85bdf1f58078b996965a9
Sandra <sandra.backstrom@gmail.com>
2017-03-19 16:22:04 +0100

    FindClosestEntity implemented

--- round-one_v2/src/main/java/se/itello/contest/ceres/teamx/HobbitBrain.java
+++ round-one_v2/src/main/java/se/itello/contest/ceres/teamx/HobbitBrain.java
@@ -6,14 +6,48 @@ import se.itello.contest.ceres.api.GameState;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 
 public class HobbitBrain implements Brain {
 
     public Collection<ShipCommand> commandsToSend(GameState state) {
+
+        System.out.println("------------------------");
+        System.out.println("Ship is at: " + state.getShipState().getPosition());
+        System.out.println("Closest entity: " + findClosestEntity(state).getPosition().toString());
+        System.out.println("------------------------");
+
         return Collections.singleton(ShipCommand.THRUST);
+
     }
 
     public String name() {
         return "Full Throttle";
     }
+
+    private EntityState findClosestEntity(GameState state) {
+        Iterator<EntityState> entities_iter = state.getContractStates().iterator();
+        Position shipPosition = state.getShipState().getPosition();
+
+        EntityState tempEntity, closestEntity = null;
+        double tempDistance, closestDistance = Double.MAX_VALUE;
+
+        while(entities_iter.hasNext()) {
+            tempEntity = entities_iter.next();
+            tempDistance = calculateDistance(shipPosition, tempEntity.getPosition());
+
+            if (tempDistance < closestDistance) {
+                closestDistance = tempDistance;
+                closestEntity = tempEntity;
+            }
+        }
+
+        return closestEntity;
+    }
+
+    // Note: Sqrt not needed as the distances will still have same relations
+    private double calculateDistance(Position a, Position b) {
+        return Math.pow((b.getX() - a.getX()), 2.0) +  Math.pow((b.getY() - a.getY()), 2.0);
+    }
 }
+
