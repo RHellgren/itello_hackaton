4efa4f0eaed7981e222aac3de68299243d16d31b
Sandra <sandra.backstrom@gmail.com>
2017-03-23 13:59:06 +0100

    Testing on HobbitBrain

--- final/src/test/java/se/itello/contest/ceres/game/Final.java
+++ final/src/test/java/se/itello/contest/ceres/game/Final.java
@@ -3,12 +3,13 @@ package se.itello.contest.ceres.game;
 import org.junit.Test;
 import se.itello.contest.ceres.api.Brain;
 import se.itello.contest.ceres.teamx.FullThrottleBrain;
+import se.itello.contest.ceres.teamx.HobbitBrain;
 
 public class Final {
 
     private static final boolean BUFFER = true;
 
-    private final Brain brain = new FullThrottleBrain();
+    private final Brain brain = new HobbitBrain();
 
     @Test
     public void run() throws Exception {
